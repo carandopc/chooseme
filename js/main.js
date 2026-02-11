0b8af4942192b9df9745704c7453ece4fb79d3a6
pcarando <paolocreas.carando@infor.com>
2026-02-11 14:14:00 +0800

    refine

--- js/main.js
+++ js/main.js
@@ -1,121 +1,65 @@
-window.onload = () => {
-  listen();
+function showOverlayFull(text, color) {
+  document.getElementById("text").innerText = text;
+  document.getElementById("overlay").style.backgroundColor = color;
+  document.getElementById("overlay").style.display = "block";
+  stopShake();
 }
 
-listen = () => {
-  var input = document.getElementById("input");
-  input.addEventListener("input", (event) => translate(event.target.value));
-  var output = document.getElementById("output");
-  output.addEventListener("click", (event) => copy(event.target.innerHTML));
+function showOverlay(text) {
+  showOverlayFull(text, '#00000090');
 }
 
-translate = (input) => {
-  var text = trim(input);
-  var tagalog = replace(text);
-  var baybayin = parse(tagalog);
-
-  var output = document.getElementById("output");
-  output.innerHTML = baybayin;
+function showOverlayCorrect() {
+  showOverlayFull(correctAnswer, '#6e006580');
+  stopShake();
+  shakeFast();
 }
 
-trim = (input) => {
-  if (input == null) {
-    input = "";
-  } else {
-    input = input.trim().toLowerCase();
+function hideOverlay() {
+  document.getElementById("overlay").style.display = "none";
+  stopShake();
+  startShake();
+}
+
+function stopShake() {
+  var images = document.getElementsByTagName("img");
+
+  for (var img of images) {
+    img.classList.remove('shake');
+    img.classList.remove('shake-fast');
   }
-
-  return input;
 }
 
-replace = (input) => {
-  // ALLOW ONLY LETTERS, PERIOD, AND COMMA
-  input = input.replace(/[^A-Za-z., ]/g, "");
-  // COMBINE VOWELS
-  input = input.replace(/e/g, "i");
-  input = input.replace(/u/g, "o");
-  // COMBINE CONSONANTS
-  input = input.replace(/v/g, "b");
-  input = input.replace(/[cq]/g, "k");
-  input = input.replace(/r/g, "d");
-  input = input.replace(/j/g, "h");
-  input = input.replace(/ng/g, "+");
-  input = input.replace(/f/g, "p");
-  input = input.replace(/[xz]/g, "s");
-  // COMBINE SPACES
-  input = input.replace(/[ ]+/g, " ");
+function startShake() {
+  var images = document.getElementsByTagName("img");
 
-  return input;
-}
-
-let special = new Map();
-special.set(" ", " ");
-special.set(",", "\u1735");
-special.set(".", "\u1736");
-
-let vowels = new Map();
-vowels.set("a", "\u1700");
-vowels.set("i", "\u1701");
-vowels.set("o", "\u1702");
-
-let consonants = new Map();
-consonants.set("b", "\u170A");
-consonants.set("k", "\u1703");
-consonants.set("d", "\u1707");
-consonants.set("g", "\u1704");
-consonants.set("h", "\u1711");
-consonants.set("l", "\u170E");
-consonants.set("m", "\u170B");
-consonants.set("n", "\u1708");
-consonants.set("+", "\u1705");
-consonants.set("p", "\u1709");
-consonants.set("s", "\u1710");
-consonants.set("t", "\u1706");
-consonants.set("w", "\u170F");
-consonants.set("y", "\u170C");
-
-let marks = new Map();
-marks.set("a", "");
-marks.set("i", "\u1712");
-marks.set("o", "\u1713");
-marks.set("+", "\u1714");
-
-parse = (input) => {
-  let count = 0;
-  let output = "";
-
-  while (count < input.length) {
-    let currentChar = input.charAt(count);
-
-    if (special.has(currentChar)) {
-      output += special.get(currentChar);
-      count++;
-    } else if (vowels.has(currentChar)) {
-      output += vowels.get(currentChar);
-      count++;
-    } else {
-      output += consonants.get(currentChar);
-      count++;
-
-      let nextChar = input.charAt(count);
-
-      if (vowels.has(nextChar)) {
-        output += marks.get(nextChar);
-        count++;
-      } else {
-        output += marks.get("+");
-      }
-    }
+  for (var img of images) {
+    img.classList.add('shake');
   }
-
-  return output;
 }
 
-copy = (input) => {
-  const text = document.createElement('textarea');
-  text.value = input;
-  document.body.appendChild(text);
-  text.select();
-  document.execCommand('copy');
-  document.body.removeChild(text);
+function shakeFast() {
+  var images = document.getElementsByTagName("img");
+
+  for (var img of images) {
+    img.classList.add('shake-fast');
+  }
 }
+
+var wrongAnswers = [
+  'oops wrong try again!',
+  'NO UR WRONG',
+  'nagkamali ka lang siguro ng click, pwede umulit',
+  'i have bent my attention towards the matter with a patience i scarcely possess, and it is with a sadness i had hoped to avoid that i perceive a confusion so artfully arranged, so barren of substance that it lingers as a pale fiction divorced from reality',
+  'nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooOoooooOOooooooooooOooooooooo!!',
+  'oh bat mo to kinclick',
+  'i stand in earnest amazement, unable to divine the true nature of so astonishing an occurrence, and i find myself arrested by a confusion so sudden that my understanding seems wrenched from its accustomed place; and in that disordered state i am shattered in the ruins of my own expectation',
+  'hmmmm hindi talaga eh',
+  'laging tatandaan na ang pagkakamali ay itinatama at hindi inuulit',
+  'i confess myself wholly confounded and, at the risk of seeming ungenerous in my doubt, must enquire whether you are indeed serious, for the claim taxes credulity beyond all reason',
+  'it is with a vexation scarce to be uttered, and with patience at last quite spent by repeated disappointment, that i am reduced to the wretched business of bearing this insufferable and detestable imposition',
+  'this is not it. this is not the one',
+  'are u sure ka na ba diyaaaan?!',
+];
+
+var correctAnswer = 'yeheeeyyyy! happy balentayms my labs! tayms two tayms tri!';
